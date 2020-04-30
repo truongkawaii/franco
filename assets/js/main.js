78c3ee72b144c12441a27c60a6e31fda139635b5
truongkawaii <60839803+truongkawaii@users.noreply.github.com>
2020-05-01 03:49:29 +0700

    up

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    responsive: {
+        320: {
+            items: 1
+        },
+        575: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        992: {
+            items: 2
+        },
+        1000: {
+            items: 3,
+
+        }
+    }
+})
---
+++ assets/js/menu.js
@@ -0,0 +1,30 @@
+const menu = {
+    init: function() {
+        this.clickShowMenu('.humber', '.header__menu', 'show__menu');
+        this.scrollMenu('.header', 'navMenu');
+    },
+    clickShowMenu: function(button, navMenu, classTogle) {
+        let btn = document.querySelector(button);
+        let menu = document.querySelector(navMenu);
+        btn.addEventListener('click', () => {
+            menu.classList.toggle(classTogle);
+        });
+    },
+    scrollMenu: function(menu, classfix) {
+        let Nav = document.querySelector(menu);
+        let heightMenu = document.querySelector(menu).offsetHeight;
+
+        window.addEventListener('scroll', () => {
+            let heightScroll = window.pageYOffset;
+            console.log(heightScroll);
+
+            if (heightScroll >= heightMenu / 2) {
+                Nav.classList.add(classfix);
+            } else {
+                Nav.classList.remove(classfix);
+            }
+
+        });
+    }
+}
+menu.init();
